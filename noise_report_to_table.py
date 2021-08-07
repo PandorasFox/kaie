0b1627b5dfc7b6672f6610595388c32cc2db7fc9
Persephone <persephone@fox.mom>
2021-08-07 14:55:50 -0700

    update table export script & regenerate md tables

--- noise_report_to_table.py
+++ noise_report_to_table.py
@@ -1,21 +1,33 @@
 import json
+import sys
+
 with open('noise_report.json', 'r') as infile:
     noise_report = json.load(infile)
 
-#print('|Noise#|Days Available|Easy|Normal|Hard|Ultimate|')
-#print('| :--: | :----------: | :: | :--: | :: | :----: |')
+try:
+    days_mode = (sys.argv[1] == 'days')
+except IndexError:
+    days_mode = False
 
-print('|Noise#|Days Available|')
-print('| :--: | :----------: |')
+outfile_name = f'noise_report{"_days" if days_mode else ""}.md'
+with open(outfile_name, 'w') as outfile:
+    if days_mode:
+        outfile.write('|Noise#|Days Available|\n')
+        outfile.write('| :--: | :----------: |\n')
+    else:
+        outfile.write('|Noise#|Days Available|Easy|Normal|Hard|Ultimate|\n')
+        outfile.write('| :--: | :----------: | :: | :--: | :: | :----: |\n')
 
-for num, noise in noise_report.items():
-    print(f'|{num}|{noise["days"]}|', end="")
-    #for difficulty in ['easy', 'normal', 'hard', 'ultimate']:
-    #    if type(noise['pins']) == str:
-    #        pin = noise['pins']
-    #        rate = noise['odds']
-    #    else:
-    #        pin = noise['pins'][difficulty]
-    #        rate = noise['odds'][difficulty]
-    #    print(f'{pin} @ {rate}%', end='|')
-    print('\n', end='')
+
+    for num, noise in noise_report.items():
+        outfile.write(f'|{num}|{noise["days"]}|')
+        if not days_mode:
+            for difficulty in ['easy', 'normal', 'hard', 'ultimate']:
+                if type(noise['pins']) == str:
+                    pin = noise['pins']
+                    rate = noise['odds']
+                else:
+                    pin = noise['pins'][difficulty]
+                    rate = noise['odds'][difficulty]
+                outfile.write(f'{pin} @ {rate}%|')
+        outfile.write('\n')
